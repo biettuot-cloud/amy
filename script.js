ee73c5a394408a0c378c2af6df0822a4b23f13e9
biettuot-cloud <nguyenha.lc13@gmail.com>
2024-09-16 13:34:15 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Thêm một thông báo khi người dùng nhấn vào nút "About"
+document.querySelector('nav ul li a[href="#"]').addEventListener('click', function() {
+    alert('Welcome to the About section!');
+});
